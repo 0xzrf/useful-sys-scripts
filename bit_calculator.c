9bc915498c1b53d3d3378f5afd4449bfe0753914
Zeref <zeref18.drgnl@gmail.com>
2026-04-30 16:27:13 +0530

    added check_byte value

--- bit_calculator.c
+++ bit_calculator.c
@@ -15,7 +15,8 @@ int starts_with(const char *arg, const char *prefix);
 int is_equal(const char *left, const char *right);
 int return_decimal(const char *arg);
 void check_sign_flag(const char *sign, char *sign_flag);
-void check_byte_value(const char *bytes, long *byte_limit);
+void check_byte_value(const char *bytes, char sign, long *upper_limit,
+                      long *lower_limit);
 
 // a script that helps you do arithmatic on either of hex, decimals or bits
 // The script does assume that you send the values in little-endian format(the
@@ -58,9 +59,42 @@ int is_equal(const char *left, const char *right) {
   return strcmp(left, right) != 0 ? 1 : 0;
 }
 
-void check_bytes_value(const char *bytes, long *byte_limit) {
-  if (!starts_with(bytes, "1") || !starts_with(bytes, "2") ||
-      !starts_with(bytes, "4") || !starts_with(bytes, "8")) {
+void check_bytes_value(const char *bytes, char sign, long *upper_limit,
+                       long *lower_limit) {
+  long max_val = 0;
+
+  if (is_equal(bytes, "1")) {
+    max_val = 0xFFL; // 255
+  } else if (is_equal(bytes, "2")) {
+    max_val = 0xFFFFL; // 65,535
+  } else if (is_equal(bytes, "4")) {
+    max_val = 0xFFFFFFFFL; // 4,294,967,295
+  } else if (is_equal(bytes, "8")) {
+    max_val = 0xFFFFFFFFFFFFFFFFL; // 18,446,744,073,709,551,615
+  } else {
+    exit(ERR_EXIT_CODE);
+  }
+
+  if (sign == 1) { // signed
+    // For signed n-bit value:
+    //   range is from -2^(n-1) to 2^(n-1)-1
+    int bits = 0;
+    if (is_equal(bytes, "1"))
+      bits = 8;
+    else if (is_equal(bytes, "2"))
+      bits = 16;
+    else if (is_equal(bytes, "4"))
+      bits = 32;
+    else if (is_equal(bytes, "8"))
+      bits = 64;
+    else
+      exit(ERR_EXIT_CODE);
+    long half = 1L << (bits - 1);
+    *upper_limit = half - 1;
+    *lower_limit = -half;
+  } else { // unsigned
+    *upper_limit = max_val;
+    *lower_limit = 0;
   }
 }
 
