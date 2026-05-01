f2380139a377d8bdd4979cd1881f16cee31999ad
Zeref <zeref18.drgnl@gmail.com>
2026-05-01 21:34:05 +0530

    added smth

--- bit_calculator.c
+++ bit_calculator.c
@@ -21,17 +21,23 @@ void check_byte_value(const char *bytes, char sign, long *upper_limit,
 // a script that helps you do arithmatic on either of hex, decimals or bits
 // The script does assume that you send the values in little-endian format(the
 // least significant value at the left-most side)
+// Usage: ./bit_calculator --[signed | unsigned] --[1 | 2 | 4 | 8]
+// <left_operand> [* | + | / | -] <right_operand>
 int main(int argc, char *argv[]) {
   const char *sign = argv[2];
+  const char *bytes = argv[3];
   // the sign of the operands
   char is_signed = 0;
   // the results should't go above this limit after the calculation
-  long limit = 0;
+  long upper_limit = 0;
+  long lower_limit = 0;
 
   check_sign_flag(sign, &is_signed);
+  check_byte_value(bytes, is_signed, &upper_limit, &lower_limit);
 
-  const char *left_operand = argv[3];
-  const char *right_operand = argv[4];
+  const char *left_operand = argv[4];
+  const char *operation = argv[5];
+  const char *right_operand = argv[6];
 }
 
 int get_arg_decimal(const char *arg) {
